a1b7f9a0e17ea19e29877fce32b1476ede0d4ace
sobek1985 <sobek1985@gmail.com>
2016-03-16 13:48:57 +0000

    Wire up animals listing

---
+++ MikeRobbins.SUGCON.Beacons/Controllers/AnimalsController.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc.Presentation;
+
+namespace MikeRobbins.SUGCON.Beacons.Controllers
+{
+    public class AnimalsController: SitecoreController
+    {
+        public ViewResult OurAnimals()
+        {
+            var datasource = RenderingContext.Current.Rendering.Item;
+         
+            return View("/Views/Components/AnimalsCTA.cshtml", datasource.Children);
+        }
+
+    }
+}
