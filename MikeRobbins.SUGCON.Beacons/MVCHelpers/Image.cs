d735797577db81c794d76439e728128183ecd5a3
sobek1985 <sobek1985@gmail.com>
2016-03-18 13:46:55 +0000

    Add animal controller

---
+++ MikeRobbins.SUGCON.Beacons/MVCHelpers/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace MikeRobbins.SUGCON.Beacons.Website.MVCHelpers
+{
+    public static class Image
+    {
+        public static string GetMediaUrl(this Sitecore.Mvc.Helpers.SitecoreHelper sitecoreHelper, string fieldName)
+        {
+            return GetMediaUrl(sitecoreHelper, fieldName, sitecoreHelper.CurrentItem);
+        }
+
+        public static string GetMediaUrl(this Sitecore.Mvc.Helpers.SitecoreHelper sitecoreHelper, string fieldName, Item item)
+        {
+            MediaItem mediaItem = item.Fields[fieldName].Item;
+            return Sitecore.Resources.Media.MediaManager.GetMediaUrl(mediaItem);
+        }
+    }
+}
