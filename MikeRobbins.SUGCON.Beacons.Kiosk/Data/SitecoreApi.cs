17be2bfcbbcb2fc6d75a7d7d8b91e878e4ed42fb
sobek1985 <sobek1985@gmail.com>
2016-03-29 13:35:05 +0100

    Add Sitecore SSC Authentication

---
+++ MikeRobbins.SUGCON.Beacons.Kiosk/Data/SitecoreApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Windows.Storage.Streams;
+using Windows.Web.Http;
+
+
+namespace MikeRobbins.SUGCON.Beacons.Kiosk.Data
+{
+    public class SitecoreApi
+    {
+        public async void Authenticate()
+        {
+
+            using (var client = new HttpClient())
+            {
+                var authDetails = BuildJsonAuthDetails();
+
+                var authResult = await client.PostAsync(new Uri("https://SUGCON/sitecore/api/ssc/auth/login"), authDetails);
+
+                authResult.EnsureSuccessStatusCode();
+
+                string uniqueIdentifier = "test@test.com"; //Pass this in from UI
+
+                var itemResult = await client.GetAsync(new Uri("https://sugcon/sitecore/api/ssc/MikeRobbins-SUGCON-Beacons-Website-Controllers/person/" + uniqueIdentifier));
+
+                itemResult.EnsureSuccessStatusCode();
+
+                var jsonContent = await itemResult.Content.ReadAsStringAsync() as dynamic;
+            }
+        }
+
+        private IHttpContent BuildJsonAuthDetails()
+        {
+            IHttpContent content = new HttpStringContent(
+                    "{ \"domain\": \"" + "sitecore" +
+                    "\" , \"username\": \"admin" +
+                    " \", \"password\": \"b\"}",
+                    UnicodeEncoding.Utf8,
+                    "application/json");
+
+            return content;
+        }
+    }
+}
---
+++ MikeRobbins.SUGCON.Beacons/IoC/SitecoreDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MikeRobbins.SUGCON.Beacons.Website.IoC
+{
+    public class SitecoreDependencyResolver : IDependencyResolver
+    {
+
+        private IDependencyResolver wrappedResolver;
+
+        public SitecoreDependencyResolver(IDependencyResolver wrappedResolver)
+        {
+            this.wrappedResolver = wrappedResolver;
+        }
+        
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return this.wrappedResolver.GetService(serviceType);
+            }
+            catch
+            {
+                return Sitecore.Mvc.Helpers.TypeHelper.CreateObject<IController>(serviceType, new object[0]);
+            }
+
+        }
+        
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.wrappedResolver.GetServices(serviceType);
+        }
+
+    }
+}
